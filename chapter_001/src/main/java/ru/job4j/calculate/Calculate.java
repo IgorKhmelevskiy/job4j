c8dc19910313f3f18c35fe594d2385a2a0430e0e
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-07-19 17:29:35 +0300

    2.2. Импорт Maven проекта

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,4 +12,14 @@ public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+
+	/**
+	 * Method echo.
+	 * @param name Igor Khmelevskiy
+	 * @return Echo plus Igor Khmelevskiy
+	 */ 
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for echo task.
+ *
+ * @author Igor Khmelevskiy (mailto:igorkkhm@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+ package ru.job4j.calculate;
