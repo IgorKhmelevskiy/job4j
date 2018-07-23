6cbfeb286283490396d61d041161a3dec4a44335
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-07-23 21:26:12 +0300

    1. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/profession/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/profession/Doctor.java
@@ -2,4 +2,6 @@ package ru.job4j.profession;
 
 public class Doctor extends Profession {
     public static void cure(String[] args) {};
+    public Diagnose heal(Patient patient) {
+    };
 }
--- chapter_002/src/main/java/ru/job4j/profession/Profession.java
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -3,4 +3,8 @@ package ru.job4j.profession;
 public class Profession {
     public String name;
     public String profession;
+
+    public String getName() {
+        return name;
+    }
 }
