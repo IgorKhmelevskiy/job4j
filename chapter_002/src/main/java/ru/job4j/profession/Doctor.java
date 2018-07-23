9b9ae4ca67e11ea0b83cb07c12e6dea141c3ca3f
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-07-23 23:28:24 +0300

    Аддон к Doctor.class

--- chapter_002/src/main/java/ru/job4j/profession/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/profession/Doctor.java
@@ -2,6 +2,6 @@ package ru.job4j.profession;
 
 public class Doctor extends Profession {
     public static void cure(String[] args) {};
-    public Diagnose heal(Patient patient) {
-    };
+   /* public Diagnose heal(Patient patient) {
+    };*/
 }
