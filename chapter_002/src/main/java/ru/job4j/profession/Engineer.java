faf44ed0491c4c8365c62e739d6f3a0a4ef8e613
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-08-06 13:55:46 +0300

    3. Профессии посление изменения

--- chapter_002/src/main/java/ru/job4j/profession/Engineer.java
+++ chapter_002/src/main/java/ru/job4j/profession/Engineer.java
@@ -2,5 +2,5 @@ package ru.job4j.profession;
 
 public class Engineer extends Profession {
     public static void buildHouse(String[] args) {};
-    public House build(House house) {};
+ /*  public House build(House house) {};*/
 }
--- chapter_002/src/main/java/ru/job4j/profession/Teacher.java
+++ chapter_002/src/main/java/ru/job4j/profession/Teacher.java
@@ -2,5 +2,5 @@ package ru.job4j.profession;
 
 public class Teacher extends Profession {
     public static void teach(String[] args) {};
-    public Discipline teachStudent(Student student) {};
+ /*   public Discipline teachStudent(Student student) {};*/
 }
