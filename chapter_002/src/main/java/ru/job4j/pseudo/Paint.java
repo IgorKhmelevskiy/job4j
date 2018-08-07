4f12bf884798e753d8bb288e28b1ff1eae2fd7bf
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-08-07 12:59:40 +0300

    4. Используя шаблон проектирования - стратегию [#786]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,12 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Igor Khmelevskiy(mailto(igorkkhm@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    public static void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
@@ -0,0 +1,10 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Igor Khmelevskiy(mailto:igorkkhm@gmail.com)
+ * @version $Id#
+ * @since 0.1
+ */
+public interface Shape {
+    String draw();
+}
