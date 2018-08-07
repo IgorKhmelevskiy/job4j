a62cbe3a65f83ce9559b2fc4713024e04979ae63
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-08-07 13:10:02 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.pseudo;
 
-import  org.junit.Test;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
@@ -12,23 +14,29 @@ import static org.junit.Assert.*;
  * @since 0.1
  */
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(new StringBuilder().append("++++").append("+     +").append("+     +").append("++++").append(System.lineSeparator()).toString()));
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(new StringBuilder().append("+").append("++").append("+ +").append("+  +").append("+++++").append(System.lineSeparator()).toString()));
-        System.setOut(stdout);
     }
 }
