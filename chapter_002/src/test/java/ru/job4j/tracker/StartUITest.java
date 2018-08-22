85b4349b3606f65922dd8ad5a75a0101aaf91851
Igor Khmelevskiy <mailtp:igkhm@rambler.ru>
2018-08-22 14:27:42 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,8 @@ package ru.job4j.tracker;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class StartUITest {
     @Test
@@ -31,5 +33,18 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name 2"));
         assertThat(tracker.findAll().length, is(1));
+    }
+
+    @Test
+    public void whenIfShowAllThenPrintAll() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name 1", "test description 1", 123L));
+        Item item1 = tracker.add(new Item("test name 2", "test description 2", 1234L));
+        Item item2 = tracker.add(new Item("test name 3", "test description 3", 1234L));
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        tracker.findAll();
+        assertThat(new String(out.toByteArray()), is(new StringBuilder().append("Заявка №%d: Имя: test name 1, Описание: test description 1").append("Заявка №%d: Имя: test name 2, Описание: test description 2").append("Заявка №%d: Имя: test name 3, Описание: test description 3").toString()));
     }
 }
